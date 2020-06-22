1ae4241b281722c856dc4d86997befb663adb1fc
sebastianocostanzo <sebastianocostanzo@hotmail.com>
2020-06-22 16:17:56 -0700

    removed hardcoded namespace reference for unmanaged deploy

---
+++ force-app/main/default/lwc/b2olocationStatus/b2olocationStatus.js
@@ -0,0 +1,24 @@
+import { LightningElement, wire, track } from 'lwc';
+import { getListUi } from 'lightning/uiListApi'; 
+
+// This is a beta functionality that is not working currently in Summer '20
+// Use at your own risk
+//import OFFICE_LOCATION_OBJECT from '@salesforce/schema/Location__c';
+
+export default class B2olocationStatus extends LightningElement {
+    @track sobjectResult;
+    @wire(getListUi, {
+        objectApiName: 'Location__c',
+        listViewApiName: 'All'
+    })
+    wiredlistView({
+        error,
+        data
+    }) {
+        if (data) {
+            this.sobjectResult = data.records.records;
+        } else if (error) {
+            this.error = error;
+        }
+    }
+}
